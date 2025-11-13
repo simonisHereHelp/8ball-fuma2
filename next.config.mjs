8e4c754e84f197d9b72b0f8c65bb279eea287a38
sc <99.cent.bagel@gmail.com>
2025-11-13 11:08:08 +0800

    to resolve next/img error, add hostname in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        // 如果需要，也可以指定 pathnamePattern
+        // pathnamePattern: '/uc/**', 
+      },
+    ],
+  },
 };
 
 export default withMDX(config);
